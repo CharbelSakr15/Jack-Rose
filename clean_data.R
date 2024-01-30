fbe7c2fe8dd73ed57d9a1a1366098c38555af07e
mrsalimx <156706656+mrsalimx@users.noreply.github.com>
2024-01-30 14:05:24 +0100

    Improved code and added more comments

--- clean_data.R
+++ clean_data.R
@@ -1,30 +1,47 @@
+### Data cleaning and preparation for the analysis ###
+
+
 library(stringr)
 titanic <- read.csv("titanic.csv")
-#head(titanic)
-View(titanic)
+
 # Extract and store titles from 'Name' into new 'Title' column         
 titanic$Title <- str_extract(titanic$Name, "(Mr\\.|Mrs\\.|Mse\\.|Ms\\.|Miss\\.|Mlle\\.|Master\\.)")
 table(titanic$Title)
-#Encode the variables 'Survived', 'Sex', 'Embarked' as factors
+# Encode the variables 'Survived', 'Sex', 'Embarked' as factors
 titanic$Survived <- factor(titanic$Survived)
 titanic$Sex <- factor(titanic$Sex)
 titanic$Embarked <- factor(titanic$Embarked)
 
-#Convert the variable 'Pclass' to an ordered factor.
+# Convert the variable 'Pclass' to an ordered factor.
 titanic$Pclass <- factor(titanic$Pclass, ordered = TRUE)
 
- # Impute missing values in 'Age' based on 'Title'
-titanic$Age <- ave(titanic$Age, titanic$Title, FUN = function(x) ifelse(is.na(x), median(x, na.rm = TRUE), x))
+# Assuming we have a data frame named 'titanic' with columns 'Age' and 'Title'
 
-# Backbord oder Steuerbord
+# We use the 'ave' function to impute missing values in 'Age' based on 'Title'
+titanic$Age <- ave(
+  titanic$Age,                   # The variable to be imputed
+  titanic$Title,                 # The grouping variable ('Title' in this case)
+  FUN = function(x) {            # The imputation function
+    ifelse(is.na(x),             # If the value is missing
+           median(x, na.rm = TRUE),  # Impute with the median of the available values
+           x)                     # Otherwise, leave the value unchanged
+  }
+)
+
+# Here, the 'ave' function is used to impute missing values in 'Age' based on 'Title'.
+# If the 'Age' value is missing, it uses the median of the available 'Age' values for the corresponding 'Title' group.
+# Otherwise, it leaves the value unchanged.
+
+
+# Port or starboard
 titanic$Side <- ifelse(as.numeric(str_extract(titanic$Cabin, "[0-9]+")) %% 2 == 1, "Steuerbord", "Backbord")
 
-# Deck: Vorangehender Buchstabe der Kabinennummer
+# Deck: Initial letter of the cabin number
 titanic$Deck <- substr(titanic$Cabin, 1, 1)
 
-# Setze Einträge mit unbekannter Kabinennummer auf NA
+# Set entries with unknown cabin numbers to NA
 titanic$Side[titanic$Cabin == ""] <- NA
 titanic$Deck[titanic$Cabin == ""] <- NA
 
-# Entferne die gewünschten Variablen 
+# Remove the desired variables
 titanic <- subset(titanic, select = -c(Name, Ticket, Cabin))
