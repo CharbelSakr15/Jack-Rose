58dda6147ab5c7801eb92c9ee5ee04194796aa2e
mrsalimx <156706656+mrsalimx@users.noreply.github.com>
2024-01-30 11:56:19 +0100

    Changes name to clean_data, adds functionality

---
+++ clean_data.R
@@ -0,0 +1,30 @@
+library(stringr)
+titanic <- read.csv("titanic.csv")
+#head(titanic)
+View(titanic)
+# Extract and store titles from 'Name' into new 'Title' column         
+titanic$Title <- str_extract(titanic$Name, "(Mr\\.|Mrs\\.|Mse\\.|Ms\\.|Miss\\.|Mlle\\.|Master\\.)")
+table(titanic$Title)
+#Encode the variables 'Survived', 'Sex', 'Embarked' as factors
+titanic$Survived <- factor(titanic$Survived)
+titanic$Sex <- factor(titanic$Sex)
+titanic$Embarked <- factor(titanic$Embarked)
+
+#Convert the variable 'Pclass' to an ordered factor.
+titanic$Pclass <- factor(titanic$Pclass, ordered = TRUE)
+
+ # Impute missing values in 'Age' based on 'Title'
+titanic$Age <- ave(titanic$Age, titanic$Title, FUN = function(x) ifelse(is.na(x), median(x, na.rm = TRUE), x))
+
+# Backbord oder Steuerbord
+titanic$Side <- ifelse(as.numeric(str_extract(titanic$Cabin, "[0-9]+")) %% 2 == 1, "Steuerbord", "Backbord")
+
+# Deck: Vorangehender Buchstabe der Kabinennummer
+titanic$Deck <- substr(titanic$Cabin, 1, 1)
+
+# Setze Einträge mit unbekannter Kabinennummer auf NA
+titanic$Side[titanic$Cabin == ""] <- NA
+titanic$Deck[titanic$Cabin == ""] <- NA
+
+# Entferne die gewünschten Variablen 
+titanic <- subset(titanic, select = -c(Name, Ticket, Cabin))
