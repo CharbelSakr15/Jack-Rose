a0817a3764d3f2b67ee7161a6dd3938a38896ee1
Henry <huick.henry@googlemail.com>
2024-02-02 13:46:09 +0100

    Adds description and missing task, clarifies comments
    Added a more informative description as header.
    Changed the order of steps and added comments that clarified the steps without being to verbose.
    Removed working artefacts and overly verbose comments.
    Added missing subtask to save the cleaned dataset as distinct file.
    Added the cleaned dataset to repo.

--- clean_data.R
+++ clean_data.R
@@ -1,42 +1,42 @@
-### Data cleaning and preparation for the analysis ###
-
+### This script contains the code to clean and prepare the Titanic dataset
+### for analysis, following a specific set of tasks provided in an university
+### assignment. The dataset is assumed to be a file named 'titanic.csv'.
+### The cleaned dataset will be stored in a new file named 'titanic_clean.csv'.
 
 library(stringr)
-titanic <- read.csv("titanic.csv")
+titanic <- read.csv("titanic.csv") # Load the dataset as a data frame
 
-# Extract and store titles from 'Name' into new 'Title' column         
-titanic$Title <- str_extract(titanic$Name, "(Mr\\.|Mrs\\.|Mse\\.|Ms\\.|Miss\\.|Mlle\\.|Master\\.)")
-table(titanic$Title)
 # Encode the variables 'Survived', 'Sex', 'Embarked' as factors
 titanic$Survived <- factor(titanic$Survived)
-titanic$Sex <- factor(titanic$Sex)
+titanic$Sex      <- factor(titanic$Sex)
 titanic$Embarked <- factor(titanic$Embarked)
 
 # Convert the variable 'Pclass' to an ordered factor.
 titanic$Pclass <- factor(titanic$Pclass, ordered = TRUE)
 
-# Assuming we have a data frame named 'titanic' with columns 'Age' and 'Title'
-
-# We use the 'ave' function to impute missing values in 'Age' based on 'Title'
-titanic$Age <- ave(
-  titanic$Age,                   # The variable to be imputed
-  titanic$Title,                 # The grouping variable ('Title' in this case)
-  FUN = function(x) {            # The imputation function
-    ifelse(is.na(x),             # If the value is missing
-           median(x, na.rm = TRUE),  # Impute with the median of the available values
-           x)                     # Otherwise, leave the value unchanged
-  }
+# Extract and store titles from 'Name' into new 'Title' column
+titanic$Title <- str_extract(
+  titanic$Name, 
+  "(Mr\\.|Mrs\\.|Mse\\.|Ms\\.|Miss\\.|Mlle\\.|Master\\.)"
 )
 
-# Here, the 'ave' function is used to impute missing values in 'Age' based on 'Title'.
-# If the 'Age' value is missing, it uses the median of the available 'Age' values for the corresponding 'Title' group.
-# Otherwise, it leaves the value unchanged.
+# Impute the missing values in 'Age' with the median of the available values
+# for the corresponding 'Title' group
+titanic$Age <- ave(
+  titanic$Age, titanic$Title, 
+  FUN = function(x) {ifelse(is.na(x), median(x, na.rm = TRUE), x)}
+)
 
+# Assigning port or starboard labels to the 'Side' variable depending on the
+# parity of the number in the 'Cabin' variable (using regular expressions)
+titanic$Side <- ifelse(
+  as.numeric(str_extract(titanic$Cabin, "[0-9]+")) %% 2 == 1,
+  "Steuerbord",
+  "Backbord"
+)
 
-# Port or starboard
-titanic$Side <- ifelse(as.numeric(str_extract(titanic$Cabin, "[0-9]+")) %% 2 == 1, "Steuerbord", "Backbord")
-
-# Deck: Initial letter of the cabin number
+# Assigning deck labels to the 'Deck' variable based on the first letter of the
+# 'Cabin' variable
 titanic$Deck <- substr(titanic$Cabin, 1, 1)
 
 # Set entries with unknown cabin numbers to NA
@@ -44,4 +44,7 @@ titanic$Side[titanic$Cabin == ""] <- NA
 titanic$Deck[titanic$Cabin == ""] <- NA
 
 # Remove the desired variables
-titanic <- subset(titanic, select = -c(Name, Ticket, Cabin))
+titanic <- subset(titanic, select = -c(PassengerId, Name, Ticket, Cabin))
+
+# Save the cleaned dataset to a new file
+write.csv(titanic, "titanic_clean.csv", row.names = FALSE)
