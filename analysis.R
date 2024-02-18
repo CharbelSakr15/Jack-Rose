aa6f03f0be7d2c1769397f05ceead85f65bad5ec
Henry <huick.henry@googlemail.com>
2024-02-18 16:22:52 +0100

    Adds alluvial plot, adds imports and improves box_plot function

--- analysis.R
+++ analysis.R
@@ -1,8 +1,10 @@
 ### This file contains functions to analyze the Titanic dataset, following a 
 ### specific set of tasks provided in an university assignment.
 
+library(ggplot2)
+library(ggalluvial)
+library(GGally)
 source("utils.R")
-
 
 # Task 2.a.i:
 # This function computes descriptive statistics for numerical variables in a
@@ -78,7 +80,7 @@ describe_categoricals = function(df) {
             counts, frequencies, nac, 
             row.names = levels(x)
         )
-        colnames(stats) = c("Count", "Frequency", "NA")
+        colnames(stats) = c("Count", "Freq", "NA")
 
         return(stats)
     })
@@ -151,8 +153,8 @@ contingency_coefficient = function(x, y, correct = TRUE) {
 #   The point-biserial correlation coefficient
 point_biserial_correlation = function(x, y, bias = TRUE) {
     # Sanity checks
-    is_dichotomous(y)
-    is_numeric(x)
+    is_dichotomous(x)
+    is_numeric(y)
     have_same_length(x, y)
 
     # Drop missing values
@@ -161,8 +163,8 @@ point_biserial_correlation = function(x, y, bias = TRUE) {
     y = y[complete]
 
     # Mask the binary variable
-    mask0 = y == levels(y)[1]
-    mask1 = y == levels(y)[2]
+    mask0 = x == levels(x)[1]
+    mask1 = x == levels(x)[2]
 
     # Compute the number of observations
     n0 = sum(mask0)
@@ -170,9 +172,9 @@ point_biserial_correlation = function(x, y, bias = TRUE) {
     n  = n0 + n1
 
     # Compute means and standard deviations
-    M0 = mean(x[mask0], na.rm = TRUE)
-    M1 = mean(x[mask1], na.rm = TRUE)
-    sn = sqrt(1 / (n - bias) * sum((x - mean(x))^2))
+    M0 = mean(y[mask0], na.rm = TRUE)
+    M1 = mean(y[mask1], na.rm = TRUE)
+    sn = sqrt(1 / (n - bias) * sum((y - mean(y))^2))
 
     # Compute the point-biserial correlation coefficient
     rpb = (M1 - M0) / sn * sqrt(n0 * n1 / n^2)
@@ -235,6 +237,122 @@ visualize_categoricals = function(df, x, y, z = NULL, w = NULL) {
     return(plot)
 }
 
+
+# This function creates a factorized frequency table for the titanic data frame.
+# It only retains the variables: 'Pclass', 'Sex', 'Age', and 'Survived'.
+# The function returns a data frame with the frequency of each combination of
+# these categories.
+# 
+# Parameters:
+#   titanic: data frame
+#      The data frame to be analyzed
+#
+# Returns:
+#   A data frame with the factorized variables counts
+factorize_categoricals = function(titanic, factors) {
+    # Sanity checks
+    if (!is.data.frame(titanic)) {
+        stop("The input must be a data frame")
+    }
+    if (!("Survived" %in% colnames(titanic))) {
+        stop("The 'Survived' variable must be in the data frame")
+    }
+    if (!all(factors %in% colnames(titanic))) {
+        stop("The data frame does not contain all the specified factors")
+    }
+
+    titanic_freq = data.frame(titanic)
+    if ("Age" %in% factors) {
+        titanic_freq$Age = ifelse(titanic_freq$Age < 14, "Child", "Adult")
+    }
+    if ("Pclass" %in% factors) {
+        titanic_freq$Pclass = ifelse(
+            titanic_freq$Pclass == 1, "1st", 
+            ifelse(titanic_freq$Pclass == 2, "2nd", "3rd")
+        )
+    }
+    titanic_freq$Survived = ifelse(titanic_freq$Survived == 1, "Yes", "No")
+
+    titanic_freq = table(titanic_freq[, c(factors, "Survived")])
+    titanic_freq = as.data.frame(titanic_freq)
+    colnames(titanic_freq) = c(factors, "Survived", "Freq")
+
+    return (titanic_freq)
+}
+
+# This function creates an alluvial diagram for the titanic data set. The 
+# input data frame must contain the factorized categorical variables produces by
+# the factorize_categoricals function.
+# The diagram visualizes the frequency of each combination of the categorical
+# variables, stratified by the 'Survived' variable.
+#
+# Parameters:
+#   df: data frame
+#      The data frame containing the variables to be visualized
+#   factors: character vector
+#      The names of the categorical variables to be visualized
+#   title: character vector
+#      The title of the plot
+#
+# Returns:
+#   A ggplot object containing the alluvial diagram
+plot_titanic_alluvial = function(df, factors, title = NULL) {
+    # Sanity checks
+    if (!is.data.frame(df)) {
+        stop("The input must be a data frame")
+    }
+    if (length(factors) > 4) {
+        stop("The factors parameter must contain less than four variables")
+    }
+    if (!("Survived" %in% colnames(df))) {
+        stop("The 'Survived' variable must be in the data frame")
+    }
+    if (!("Freq" %in% colnames(df))) {
+        stop("The data frame must contain a 'Freq' variable")
+    }
+
+    # Create the aesthetic mappings with stupido code R requires
+    n = length(factors)
+    if (n == 1) {
+        aess = aes(
+            axis1 = !!rlang::sym(factors[1]), 
+            y = Freq
+        )
+    } else if (n == 2) {
+        aess = aes(
+            axis1 = !!rlang::sym(factors[1]), 
+            axis2 = !!rlang::sym(factors[2]), 
+            y = Freq
+        )
+    } else if (n == 3) {
+        aess = aes(
+            axis1 = !!rlang::sym(factors[1]), 
+            axis2 = !!rlang::sym(factors[2]), 
+            axis3 = !!rlang::sym(factors[3]), 
+            y = Freq
+        )
+    } else if (n == 4) {
+        aess = aes(
+            axis1 = !!rlang::sym(factors[1]), 
+            axis2 = !!rlang::sym(factors[2]), 
+            axis3 = !!rlang::sym(factors[3]), 
+            axis4 = !!rlang::sym(factors[4]), 
+            y = Freq
+        )
+    }
+
+    # create the alluvial plot
+    plot = ggplot(data = df, aess) +
+        scale_x_discrete(limits = factors, expand = c(.2, .05)) +
+        geom_alluvium(aes(fill = Survived)) +
+        geom_stratum() +
+        geom_text(stat = "stratum", aes(label = after_stat(stratum))) +
+        theme_minimal() +
+        ggtitle(title[1], title[2])
+
+    return(plot)
+}
+
 # Task 2.a.vi:
 # further functions suitable for description and visualization
 
@@ -278,16 +396,26 @@ scatter_plot_matrix = function(df) {
 
 # This function creates a box plot for a numerical variable, grouped by a
 # categorical variable
-box_plot = function(df, x, y) {
+box_plot = function(df, factor1, factor2) {
     # Sanity checks
-    is_factor(y)
-    is_numeric(x)
+    if (!is.data.frame(df)) {
+        stop("The input must be a data frame")
+    }
+    if (!is.numeric(df[[factor1]])) {
+        stop("The first variable must be numeric")
+    }
+    if (!is.factor(df[[factor2]])) {
+        stop("The second variable must be a factor")
+    }
+    
+    x_data = df[[factor1]]
+    y_data = df[[factor2]]
 
     # Create the box plot
-    plot = ggplot(df, aes(x = y, y = x)) +
+    plot = ggplot(df, aes(x = y_data, y = x_data)) +
            geom_boxplot() +
-           labs(title = "Box plot of " ~ x ~ " by " ~ y,
-                x = y, y = x) +
+           labs(title = paste0("Box plot of ", factor2, " by ", factor1),
+                x = factor2, y = factor1) +
            theme_minimal()
     return(plot)
 }
